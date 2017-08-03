15c06bd50cd321fc53bb986c1e1191ed311d1451
Howard Huang <ho1919192000@gmail.com>
2017-08-03 01:06:34 -0600

    first commit

---
+++ src/app/components/profile.component.ts
@@ -0,0 +1,18 @@
+import { Component, OnInit } from '@angular/core';
+import { GithubService } from '../services/github.service';
+@Component({
+  moduleId: module.id,
+  selector: 'app-profile',
+  templateUrl: './profile.component.html',
+  styleUrls: ['./profile.component.css']
+})
+export class ProfileComponent implements OnInit {
+
+  constructor(private _githubService: GithubService) {
+    
+  }
+
+  ngOnInit() {
+  }
+
+}
---
+++ src/app/services/github.service.ts
@@ -0,0 +1,18 @@
+import { Injectable } from '@angular/core';
+import { Http, Headers } from '@angular/http';
+import 'rxjs/add/operator/map';
+import { Observable } from "rxjs/Observable"; // <- add this import to solve TS4053 
+@Injectable()
+export class GithubService {
+  private username: string;
+  private client_id = 'cd34965ee1f96446dd63';
+  private client_secret = '26ae03aeea9ce79f074e13686ab538718f8d00f3';    
+  constructor(private _http: Http) { 
+      console.log('Github Service Ready..');
+      this.username = 'ho1919192000';
+  }
+ getUser(){
+      return this._http.get('http://api.github.com/users/'+this.username+'?client_id='+this.client_id+'&client_secret='+this.client_secret).map(res => res.json());
+  }
+
+}
